a997ed9a664cb929e59f8e2d07f6ffff4c85a1cf
Ezz El-Din Yasser Mohammed <ezzeldin.yasser@hotmail.com>
2023-12-02 17:46:39 +0200

    Updated UI

--- Chemistry Laboratory Simulator/Assets/Scripts/MainMenu.cs
+++ Chemistry Laboratory Simulator/Assets/Scripts/MainMenu.cs
@@ -1,8 +1,7 @@
-using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
